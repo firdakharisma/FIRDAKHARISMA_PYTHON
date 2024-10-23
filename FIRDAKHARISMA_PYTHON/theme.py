c10b0a8ea3d5e8e390ba004dfced600efaf91900
Firda Kharisma <56108842+firdakharisma@users.noreply.github.com>
2024-10-23 21:23:12 +0800

    Add files via upload

---
+++ FIRDAKHARISMA_PYTHON/theme.py
@@ -0,0 +1,12 @@
+import PySimpleGUI as sg
+sg.theme("DarkGreen4")
+sg.theme_text_color("#FFFF00")
+window = sg.Window(title="Profile",
+                   layout=[[sg.Text("SELAMAT DATANG DI KELAS",
+                                    font=("Arial",25,"italic","bold", "underline"))],
+                                    [sg.Text("NPM       : 2210010033")],
+                                    [sg.Text("Nama      : JAMILATUZZAHRA")],
+                                    [sg.Text("Kelas     : 5E Regular Banjarmasin")]
+                                    ],
+                                    size=(510,200),
+                                    font=("Times", 18))
